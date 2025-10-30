bd84ce449196adca7c0bf74664d02d58017489f3
Nguyen Manh Cuong <Cuong.NM225268@sis.hust.edu.vn>
2025-10-30 20:52:26 +0700

    Add DashBoard , fix Navigate

--- client/src/pages/Login-Page.jsx
+++ client/src/pages/Login-Page.jsx
@@ -1,26 +1,82 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const LoginPage = () => {
+  const [Username, setUsername] = React.useState("");
+  const [Password, setPassword] = React.useState("");
+  const [Error, setError] = React.useState("");
+  const [Message, setMessage] = React.useState("");
+  const navigate = useNavigate();
+
+
+  const handleSubmit =  async (e) => {
+    e.preventDefault();
+    //đợi test API
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: Username,
+        password: Password,
+      }),
+    });
+
+    const data = await res.json();
+
+    if(res.ok){
+      sessionStorage.setItem("loggedIn", "true");
+      setError("");
+      setMessage("Đăng nhập thành công!");
+      setTimeout(() => {
+        setMessage("");
+        navigate("/dashboard");
+        window.location.reload();  
+      }, 1000);
+    }else{
+      setError(data.error || "Đăng nhập thất bại");
+    }
+    // if (Username === "Cuong" && Password === "12345") {
+    //   localStorage.setItem("loggedIn", "true");
+    //   setError("");
+    //   setMessage("Đăng nhập thành công!");
+    //   setTimeout(() => {
+    //     setMessage("");
+    //     navigate("/");
+    //     window.location.reload();  
+    //   }, 2000);
+    // } else {
+    //   setError("Sai mật khẩu hoặc tên đăng nhập");
+    // }
+  };
+
   return (
     <div style={styles.container}>
       <div style={styles.overlay}></div>
-      <form style={styles.form}>
+      <form style={styles.form} onSubmit={handleSubmit}>
         <h2 style={styles.title}>Login</h2>
         <p style={styles.subtitle}>Login to manage your tasks efficiently</p>
 
         <input
-          type="text"
+          type="email"
           placeholder="Username"
           style={styles.input}
+          value={Username}
+          onChange={(e) => setUsername(e.target.value)}
           required
         />
         <input
           type="password"
           placeholder="Password"
           style={styles.input}
+          value={Password}
+          onChange={(e) => setPassword(e.target.value)}
           required
         />
-
+        {Error && <p style={{ color: "red" }}>{Error}</p>}
+        {Message && <p style={{ color: "green" }}>{Message}</p>}
         <button type="submit" style={styles.button}>
           Login
         </button>
