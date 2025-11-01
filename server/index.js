97a6f81b1fc0443226d40f331620a2e2363678b2
QCuong-24 <quoccuong2k4ht@gmail.com>
2025-11-01 22:06:47 +0700

    Fix package and a small change to index.js

--- server/index.js
+++ server/index.js
@@ -7,3 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server đang chạy (listening) tại http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('Chào mừng bạn đến với ứng dụng Express!');
+});
